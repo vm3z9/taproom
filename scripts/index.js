994eda15e747a833ca66792f7d8ec1801eecbc93
V.Mak <vigomak@gmail.com>
2021-02-26 09:57:52 -0800

    add in some commenting

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', (e) => {
 
+	// listen for clicks
 	document.addEventListener("click", handleBuyButton);	
 
 });
 
+/**
+ * Detect clicks on 'buy now' button
+ */
 const handleBuyButton = (e) => {
 
 	if (e.target.classList.contains("buy-btn-overlay")) {;
